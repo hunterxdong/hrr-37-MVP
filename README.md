3dbd627798a4b856623e6e08706db9cf7832a83e
hunterxdong <hunterdong123@gmail.com>
2019-05-06 11:16:32 -0700

    updated clear button to react

---
+++ README.md
@@ -0,0 +1,8 @@
+Simple budgeting tool of keeping track of who owes you money.
+
+How to start:
+npm install //installs dependencies
+npm run build //starts webpack
+npm run run //starts the server
+
+Set to run on port 5000
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,74 +3,18 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 class App extends React.Component {
-	componentDidMount() {
-		$(document).ready(function() {
-			// this is where we jquery
-			//var keyData = 'ourKey'; // going to need to make this dynamic?
-
-			$('.btn-add').on('click', function(e) {
-				console.log(e);
-				var keyData = $('.input-key').val();
-				var valueData = $('.input-value').val();
-				// write to db
-				if (valueData === '') {
-					alert('Please input a number');
-					return false;
-				}
-				if (keyData === '') {
-					alert('Please input a name');
-					return false;
-				}
-				localStorage.setItem(keyData, valueData);
-				// read from db
-				var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-				// this only displays the last one? might want to switch to html
-				// and append a div
-				// <div class="display-data-item" data-keyValue="keyData">valueData</div>
-				// if you use backticks ` you can use ${templateLiterals}
-				// TODO make this vars make sense across the app
-				$(
-					'<ul class="display-data-item" data-keyValue="' +
-						keyData +
-						'">' +
-						keyData +
-						' ' +
-						'$' +
-						valueData +
-						'<div class="deleteMe">X</div></ul>'
-				).appendTo('.container-data');
-				$('.total').html(parseInt($('.total').html()) + parseInt(valueData));
-				$('.input-key').val('');
-				$('.input-value').val('');
-			});
-
-			// update db
-			// need to expand when  more than 1 item is added
-
-			// delete item
-			$('.container-data').on('click', '.display-data-item', function(e) {
-				console.log(e);
-				var keyData = e.currentTarget.dataset.keyvalue;
-				$('.total').html(
-					parseInt($('.total').html()) - parseInt(localStorage.getItem(keyData))
-				);
-				$(this)
-					.closest('ul')
-					.remove();
-				var audio = document.getElementsByTagName('audio')[0];
-				audio.play();
-			});
-
-			// delete all?
-			$('.btn-clear').click(function() {
-				alert('please enter credit card info');
-				localStorage.clear();
-				$('.container-data').text('');
-				$('.total').html(parseInt($('.total').html()) * 0);
-			});
-		});
+	constructor(props) {
+		super(props);
+		this.state = {
+			total: 0
+		};
 	}
-
+	clickAddButton = function() {
+		this.setState({ total: this.state.total + 1 });
+	};
+	absolveAll = function() {
+		this.setState({ total: 0 });
+	};
 	render() {
 		return (
 			<div>
@@ -83,14 +27,17 @@ class App extends React.Component {
 							className="input-value"
 							placeholder="HOW MUCH?!?!?!"
 						/>
-						<button className="btn-add" onClick={this.clickAddButton}>
+						<button
+							className="btn-add"
+							onClick={this.clickAddButton.bind(this)}
+						>
 							Submit
 						</button>
-						<button className="btn-clear" onClick={this.absolveAll}>
+						<button className="btn-clear" onClick={this.absolveAll.bind(this)}>
 							Absolve All
 						</button>
 						<div className="total-owed">Total $:</div>
-						<div className="total">0</div>
+						<div className="total">{this.state.total}</div>
 					</div>
 				</div>
 				<div className="container-data" />
